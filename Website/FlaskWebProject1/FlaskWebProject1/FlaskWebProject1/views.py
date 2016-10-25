a00de5340a3c9e2ebb2f35643b16af0366d6fd66
ChielBroere <chielbroere@gmail.com>
2016-10-25 12:45:15 +0200

    Updated all

--- Website/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/views.py
+++ Website/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/views.py
@@ -1,14 +1,8 @@
 from flask import render_template
 from FlaskWebProject1 import app
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/brands/<name>')
 def brands(name):
-    
     return render_template('brands.html', name=name)
 
 
