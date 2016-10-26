bc9f2fdb02118df0fe894853a4a861538509a506
Arjen <arjenhoek1994@hotmail.com>
2016-10-26 14:47:12 +0200

    select all & select single record

--- Website/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/Database.py
+++ Website/FlaskWebProject1/FlaskWebProject1/FlaskWebProject1/Database.py
@@ -1,47 +1,56 @@
 import sqlite3
 import json
-import pprint
-
 
 print "running script"
 
 class Database(object):
 	# Contructior opens database
 	def __init__(self):
-		self.conn = sqlite3.connect("db.sqlite")
+		# test
+		a=1
+
+	def open_conn(self):
+		self.conn = sqlite3.connect("data.db")
 		self.c = self.conn.cursor()
+
+	def close_conn(self):
+		self.conn.commit()
+		self.conn.close()
 
 	# Create releavant tables 
 	# Only use if you know what you are doing!!!
 	def create_table(self):
+		self.open_conn()
 		self.c.execute("drop table coffee")
 		self.c.execute("drop table aroma")
 		self.c.execute("drop table coffee_aroma")
 		self.c.execute('CREATE TABLE coffee (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, price REAL, roast TEXT, origin TEXT, picture TEXT)')
 		self.c.execute('CREATE TABLE aroma (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
 		self.c.execute('CREATE TABLE coffee_aroma (id INTEGER PRIMARY KEY AUTOINCREMENT, coffee_id INT, aroma_id INT)')
+		self.close_conn()
+
 
 	# Instert some data into a table
 	def insert_aroma(self):
+		self.open_conn()
 		self.c.execute('insert into aroma (name) values ("Nutty")')
 		self.c.execute('insert into aroma (name) values ("Fruity")')
 		self.c.execute('insert into aroma (name) values ("Spicy")')
 		self.c.execute('insert into aroma (name) values ("Chocolate")')
-		test = "pie"
-		self.c.execute('insert into aroma (name) values ("'+ test +'")')
+		self.close_conn()
+		
 
 	# Gets json and inserts values into databse
 	def insert_coffee(self):
-		# products = Models.Product.get_all()
-		# products = Models.Product.ArrayToJson(products)
-		# return products
+		self.open_conn()
 		data = open("products.json", 'r')
 		jsonData = json.load(data)
 		# return jsonData
 		for item in jsonData:
 			# insert coffees
-			querry = 'insert into coffee (id, name, description, price, roast, origin, picture) values ({}, "{}", "{}", {}, "{}", "{}", "{}")'.format(str(item["ID"]), item["Name"], item["Description"], str(float(item["Price"])), item["Roast"], item["Origin"], item["Image"])
-			# self.c.execute(querry)
+			querry = 'insert into coffee (id, name, description, price, roast, origin, picture) \
+					values ({}, "{}", "{}", {}, "{}", "{}", "{}")'.format(str(item["ID"]), item["Name"], item["Description"], str(float(item["Price"])), item["Roast"], item["Origin"], item["Image"])
+			self.c.execute(querry)
 
 			# insert connection between aromas and coffees
 			for aroma in item["Aromas"]:
@@ -54,40 +63,72 @@ class Database(object):
 				if aroma == 'Chocolate':
 					querry = 'INSERT INTO coffee_aroma(coffee_id, aroma_id) VALUES ({}, {})'.format(item["ID"], 4)
 				self.c.execute(querry)
-
-			# self.c.execute('insert into coffee (id, name, description, price, roast, origin, picture) values ({}, "{}", "{}", {}, "{}", "{}", "{}")'.format(item["ID"], item["Name"], item["Description"], float(item["Price"]), item["Roast"], item["Origin"], item["Image"]))
-			# self.c.execute('insert into coffee (id, name, description, price, roast, origin, picture) values ('+ item["ID"] +',"'+ item["Name"] +'","'+ item["Description"] +'",'+ float(item["Price"]) +',"'+ item["Roast"] +'"'+ item["Origin"] +'","'+ item["Image"] +'")')
-			# item["ID"], item["Name"], item["Description"], float(item["Price"]), item["Roast"], item["Origin"], item["Image"]
-			# for aroma in item["Aromas"]
+		self.close_conn()
 				
-		# return item
-		
-
 	# Drop all tables, Create new table and fill them
 	def reset_database(self):
 		self.create_table()
 		self.insert_aroma()
 		self.insert_coffee()
 
-	# Get all from table
-	def get_all(self):
-		self.c.execute("select * \
+	# Get one coffee by it's id
+	def get_coffee_by_id(self, id):
+		self.open_conn()
+		self.c.execute("select *\
 						FROM coffee c \
-						INNER JOIN coffee_aroma ca on c.id = ca.coffee_id\
-						INNER JOIN aroma a on ca.aroma_id = a.id\
-						")
-		return self.c.fetchall()
+						WHERE c.id = {}\
+						".format(id))
+		result = self.c.fetchall()
+		# Convert list of tuples to list of lists so we can eddit it
+		result = [list(elem) for elem in result]
+		for coffee in result:
+			self.c.execute("select a.name \
+							FROM coffee_aroma ca \
+							INNER JOIN aroma a on ca.aroma_id = a.id \
+							WHERE ca.coffee_id = {}".format(id))
+			aromas = self.c.fetchall()
+			# convert list of tuples to list
+			aromas = [elem[0] for elem in aromas]
+			coffee.append(aromas)
+		self.close_conn()
+		return result
 
+	# Get all from table
+	def get_all(self, filter = None):
+		querry = "select *\
+				FROM coffee c"
+		if filter != None
+			querry += " WHERE "
+			
+			querry += " c.id = 1 and "
+
+		self.open_conn()
+		self.c.execute(querry)
+		result = self.c.fetchall()
+		# Convert list of tuples to list of lists so we can eddit it
+		result = [list(elem) for elem in result]
+		for coffee in result:
+			self.c.execute("select a.name \
+							FROM coffee_aroma ca \
+							INNER JOIN aroma a on ca.aroma_id = a.id \
+							WHERE ca.coffee_id = {}".format(coffee[0]))
+			aromas = self.c.fetchall()
+			# convert list of tuples to list
+			aromas = [elem[0] for elem in aromas]
+			coffee.append(aromas)
+
+		self.close_conn()
+		return result
 
 		
 db = Database()
-db.reset_database()
-# print db.insert_coffee()
+# db.reset_database()
 
 result = db.get_all()
-print(result)
+# result = db.get_coffee_by_id(5)
 
-db.conn.close()
+# print(result)
+
 
 
 print "end script"
