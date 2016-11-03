5dc6ba5c612fe5fbf396d914d64c60954f7d6c8b
ChielBroere <chielbroere@gmail.com>
2016-11-03 10:37:41 +0100

    Removed some html and json

--- website/website/views.py
+++ website/website/views.py
@@ -10,18 +10,6 @@ def products(id):
     print(id)
     return render_template('ProductDetailsViewContainer.html', id=id)
 
-@app.route('/account')
-def account():
-    return render_template('account.html')
-
-@app.route('/account/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 @app.route('/login')
 def login():
     return render_template("BaseContainerLogin.html");
