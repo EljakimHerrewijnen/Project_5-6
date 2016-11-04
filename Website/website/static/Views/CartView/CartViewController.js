0a74e346e2ce08db1ae6512dc9ceea6525c3dc4b
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-11-04 11:37:15 +0100

    edited js and controller. Messages show now.

--- Website/website/static/Views/CartView/CartViewController.js
+++ Website/website/static/Views/CartView/CartViewController.js
@@ -6,9 +6,10 @@ function buildView() {
     });
 }
 
-function storedata(product)
+function storedata(id, name, description)
 {
-    localStorage.setItem("product", product);
+    window.alert("hello")
+    localStorage.setItem("product", id, name, description);
 }
 
 function getshoppingcart()
