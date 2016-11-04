97278b738d6f63ea89a4aea85ad2442b5c7b458f
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-11-04 10:52:37 +0100

    edited cart.js and basecontainer.html

--- Website/website/static/JSHelpers/cart.js
+++ Website/website/static/JSHelpers/cart.js
@@ -1,5 +1,12 @@
-function storedata()
-{
-    
 
+function storedata(product)
+{
+    localStorage.setItem("product", product);
+}
+
+function getshoppingcart()
+{
+    var product = localStorage.getItem("product");
+    console.log(product);
+    var productobject = JSON.parse(product);
 }
