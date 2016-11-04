50e37d255aa862acded74a617c48358e05e712ba
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-11-04 11:03:52 +0100

    cart.js changed

--- Website/website/static/Views/CartView/CartViewController.js
+++ Website/website/static/Views/CartView/CartViewController.js
@@ -5,3 +5,15 @@ function buildView() {
         onViewLoad();
     });
 }
+
+function storedata(product)
+{
+    localStorage.setItem("product", product);
+}
+
+function getshoppingcart()
+{
+    var product = localStorage.getItem("product");
+    console.log(product);
+    var productobject = JSON.parse(product);
+}
