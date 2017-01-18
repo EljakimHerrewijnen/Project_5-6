130f317f34aeedc9f5a91b6e8fcefc54d44954e4
bart-rijnders <mail@bartrijnders.com>
2017-01-19 00:12:29 +0100

    Added user chart

--- website/app/static/controllers/admin/dashboard.js
+++ website/app/static/controllers/admin/dashboard.js
@@ -16,6 +16,7 @@ function adminDashboard() {
         promise = Promise.all([html, users]).then(([html, users]) =>{
             html = Handlebars.compile(html);
             container.append(html(users));
+            createChart(users);
             container.css({opacity: 0});
         });
 
@@ -32,4 +33,30 @@ function adminDashboard() {
     
     var getHtml = () => $.ajax({url: "/static/views/admin/dashboard.html"});
     var getUsers = () => $.ajax({url: "/api/admin/account"});
+
+    var createChart = (users) => {
+        Chart.defaults.global.legend.display = false;
+        var options = {
+            responsive: true,
+            maintainAspectRatio: false
+        };
+
+        var data = {
+            labels: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dev"],
+            datasets: [
+                {
+                    backgroundColor: "rgba(0,0,0,0)",
+                    borderColor: "#00bfc7",
+                    data: [52, 66, 40, 29, 50, 75, 67, 38, 55, 77, 66, 39]
+                },
+            ]
+        };
+
+        var ctx = container.find("#orderChart");
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
+    }
 }
