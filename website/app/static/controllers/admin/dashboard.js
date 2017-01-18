57318b9d23b4d22a3b65b269da01970193b28bc1
bart-rijnders <mail@bartrijnders.com>
2017-01-18 14:01:57 +0100

    initial commit for added admin page

---
+++ website/app/static/controllers/admin/dashboard.js
@@ -0,0 +1,36 @@
+viewManager.addRoute("/wishlists", () => new browseWishlistView());
+
+function browseWishlistView() {
+    var self = this;
+    var container;
+    var wishlists;
+
+    Object.defineProperty(this, "url", {
+        get : () => '/wishlists'
+    });
+
+    this.construct = function(newContainer) {
+        container = newContainer;
+        var html = getHtml();
+        var wishlists = getWishlists();
+
+        promise = Promise.all([html, wishlists]).then(([html, wishlists]) =>{
+            html = Handlebars.compile(html);
+            container.append(html(wishlists));
+            container.css({opacity: 0});
+            console.log(wishlists); 
+        });
+
+        return promise;
+    }
+
+    this.destruct = function() {
+        return container.animate({opacity:0}, 150).promise().then(() => container.remove());
+    }
+    this.transitionIn =function() {
+        container.animate({opacity: 1}, 150);
+    }
+    
+    var getHtml = () => $.ajax({url: "/static/views/browse-wishlists-view.html",contentType: "text"});
+    var getWishlists = () => $.ajax({url: "/api/wishlist" ,contentType: "text"});
+}
