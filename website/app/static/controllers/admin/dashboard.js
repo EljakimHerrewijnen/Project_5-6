7a5565a20cccc8c5405e37e79fbc39e78b444bf0
bart-rijnders <mail@bartrijnders.com>
2017-01-19 11:57:14 +0100

    Added three charts

--- website/app/static/controllers/admin/dashboard.js
+++ website/app/static/controllers/admin/dashboard.js
@@ -3,6 +3,10 @@ viewManager.addRoute("/admin/dashboard", () => new adminDashboard());
 function adminDashboard() {
     var self = this;
     var container;
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false
+    };
 
     Object.defineProperty(this, "url", {
         get : () => '/admin/dashboard'
@@ -12,14 +16,16 @@ function adminDashboard() {
         container = newContainer;
         var html = getHtml();
         var users = getUsers()
+        var products = stateManager.getProducts();
 
-        promise = Promise.all([html, users]).then(([html, users]) =>{
+        promise = Promise.all([html, users, products]).then(([html, users, products]) =>{
             html = Handlebars.compile(html);
             container.append(html(users));
             createChart(users);
+            createProductChart(products);
+            createAromaChart(products);
             container.css({opacity: 0});
         });
-
         return promise;
     }
 
@@ -56,6 +62,79 @@ function adminDashboard() {
         var myLineChart = new Chart(ctx, {
             type: 'line',
             data: data,
+            options: options
+        });
+    }
+
+    var createProductChart = function(products) {
+        Chart.defaults.global.legend.display = true;
+        var darkAmount = products.filter((x) => x.roast == "Dark").length;
+        var mediumAmount = products.filter((x) => x.roast == "Medium").length;
+        var lightAmount = products.filter((x) => x.roast == "Light").length;
+
+        var data = {
+            labels: [
+                "Dark",
+                "Medium",
+                "Light"
+            ],
+            datasets: [
+                {
+                    data: [darkAmount, mediumAmount, lightAmount],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+        };
+
+        var ctx = container.find("#productChart");
+        var myDoughnutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: data,
+            options: options,
+            animation:{
+                animateScale:true
+            }
+        });
+    }
+
+    var createAromaChart = function(products) {
+        Chart.defaults.global.legend.display = true;
+        var chocolateAmount = products.filter((x) => $.inArray("Chocolate", x.aromas)).length;
+        var nuttyAmount = products.filter((x) => $.inArray("Nutty", x.aromas)).length;
+        var fruity = products.filter((x) => $.inArray("Fruity", x.aromas)).length;
+        var spicy = products.filter((x) => $.inArray("Spicy", x.aromas)).length;
+
+        var data = {
+            labels: [
+                "Chocolate",
+                "Nutty",
+                "Fruity",
+                "Spicy"
+            ],
+            datasets: [
+                {
+                    data: [chocolateAmount, nuttyAmount, spicy, fruity],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#4BC0C0",
+                        "#FFCE56",
+                        "#36A2EB"
+                    ]
+            }]
+        }
+
+        var ctx = container.find("#aromaChart");
+        new Chart(ctx, {
+            data: data,
+            type: 'polarArea',
             options: options
         });
     }
