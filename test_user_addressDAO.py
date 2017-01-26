b16dc8c17f24686a13151567d9d64c3583462df0
ChielBroere <chielbroere@gmail.com>
2017-01-26 16:20:49 +0100

    Fix

---
+++ test_user_addressDAO.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from app.api.DAO.user_addressDAO import Create, Delete
+from app.api.database import Database
+
+class TestUserAddress(unittest.TestCase):    
+    # def test_create(self):
+    #     db = Database()
+    #     Setup.create()
+    #     result = db.get_one_querry("SELECT * FROM user_address WHERE username='useraddresstest'")        
+    #     self.assertEqual({'postal_code': '1234AB', 'house_number': 1, 'username': 'useraddresstest'}, result)
+
+    # def test_delete(self):
+    #     db = Database()
+    #     Setup.delete()
+    #     result = db.raw_get_one_querry("SELECT * FROM user_address WHERE username='useraddresstest'")        
+    #     self.assertEqual({'house_number': 1, 'postal_code': '1234AB', 'username': 'useraddresstest'}, result)
+
+    def test_create(self):
+        create = Create("4321BA", 1, "testuser2")
+        result = "{}"
+        self.assertEqual(create, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
