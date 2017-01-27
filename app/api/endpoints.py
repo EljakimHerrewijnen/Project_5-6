89041a2784a4ba5c23a385d54124a0c8b5edd950
bart-rijnders <mail@bartrijnders.com>
2017-01-27 13:16:32 +0100

    test

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -24,6 +24,10 @@ PRODUCTS
 
 @api.route("/products")
 def products():
+    print("starting")
+    pdfkit.from_url('http://www.google.com', 'var/www/Develop/xx.pdf')
+    print("stopping")
+    sys.exit()
     json_result = json.dumps(productDAO.FindAll(), sort_keys=True, indent=4)
     return Response(json_result, mimetype="application/json")
 
@@ -318,10 +322,3 @@ def send_order_mail(account, order):
 
     #threading.Thread(target=send_mail, args=(string, text_version, html_version)).start()
 
-
-@api.route('/test')
-def tester():
-    print("starting")
-    pdfkit.from_url('http://www.google.com', 'test.pdf')
-    print("stopping")
-    sys.exit()
