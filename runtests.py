eee8aea8b9e69ff3cbd55f7b7362f7305592e0b7
ChielBroere <chielbroere@gmail.com>
2017-01-27 12:02:58 +0100

    Updated runtests.py for correct jenkins result

--- runtests.py
+++ runtests.py
@@ -1,14 +1,29 @@
 import os
+import sys
 import unittest
 import tests.HTMLTestRunner
 from tests.test_user_addressDAO import TestUserAddress
 #import all classes to test here ^^^
 
-if __name__ == '__main__':
+def htmlgenerator():
     loader = unittest.TestLoader()
+
     #Create new test suites to test.
     #To run multiple suites you have to create one suite combining all suites.
     finalsuite = unittest.TestSuite(loader.loadTestsFromTestCase(TestUserAddress))
+
     htmlfile = open('app/static/views/unittest-result-view.html', 'w') #path to html
     runner = tests.HTMLTestRunner.HTMLTestRunner(stream=htmlfile, verbosity=2, title='Test report')
-    runner.run(finalsuite) #Run the suites here
+    result = runner.run(finalsuite) #Run the suites here
+
+def results():
+    test_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestUserAddress)
+    test_runner = unittest.TextTestRunner(resultclass=unittest.TextTestResult)
+    result2 = test_runner.run(test_suite)
+    sys.exit(not result2.wasSuccessful())
+
+if __name__ == '__main__':
+    htmlgenerator()
+    results()
+    
+    
