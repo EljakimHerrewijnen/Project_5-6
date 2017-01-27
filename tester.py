e41d5e249f4096c7e421ab611af41d2e631be397
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-01-27 11:50:19 +0100

    Extended Test method to test multiple times stopping when one fails

--- tester.py
+++ tester.py
@@ -2,7 +2,17 @@ from tests.testAdminDAO import AdminDAOTest
 
 def Test():
     testing = AdminDAOTest()
-
     testing.setUp()
+    successfulTest = False
+
+    for i in range(0, 5):
+        successfulTest = testing.TestAdmin()
+
+        if not(successfulTest):
+            print("F")
+            return
+
+        print("P", end = "")
+    print("\n\nTest Successful!!")
 
 Test()
